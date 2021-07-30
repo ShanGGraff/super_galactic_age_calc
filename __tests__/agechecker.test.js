c40b41e3339efb94fb34d39f8a251981dbee74a9
Shane Graff <copellius@gmail.com>
2021-07-29 23:19:34 -0700

    Add test to correctly test age of person. Add beforeEach to clean up code

--- __tests__/agechecker.test.js
+++ __tests__/agechecker.test.js
@@ -1,5 +1,13 @@
 import Person from '../src/js/person.js'
 
 describe('Person', () => {
+  let person;
 
+  beforeEach(() => {
+    person = new Person(12);
+  });
+
+  test("should create a person object with the person's age", () => {
+    expect(person.age).toEqual(12);
+  });
 });
