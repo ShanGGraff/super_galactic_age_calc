1d8ecd9631bb6128e5d3b60b11feff96d0c2cfca
Shane Graff <copellius@gmail.com>
2021-07-30 12:14:33 -0700

    Add life expectancy method for Mercury test

--- __tests__/agechecker.test.js
+++ __tests__/agechecker.test.js
@@ -11,16 +11,19 @@ describe('Person', () => {
     expect(person.age).toEqual(12);
     expect(person.lifeExpectancy).toEqual(72);
   });
-  test("should return the age of person in Mercury years,", () => {
+  test("should return the age of person in Mercury years", () => {
     expect(person.ageToMercuryYears()).toEqual(2.88);
   });
-  test("should return the age of person in Venus years,", () => {
+  test("should return the age of person in Venus years", () => {
     expect(person.ageToVenusYears()).toEqual(7.44);
   });
-  test("should return the age of person in Mars years,", () => {
+  test("should return the age of person in Mars years", () => {
     expect(person.ageToMarsYears()).toEqual(22.56);
   });
-  test("should return the age of person in Jupiter years,", () => {
+  test("should return the age of person in Jupiter years", () => {
     expect(person.ageToJupiterYears()).toEqual(142.32);
+  });
+  test("should return the life expectancy on Mercury", () => {
+    expect(person.lifeExpectancyOnMercury()).toEqual(14.2);
   });
 });
