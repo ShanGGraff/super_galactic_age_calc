ad14b29f670164f34df98f303d502434f81e6a71
Shane Graff <copellius@gmail.com>
2021-07-29 23:27:18 -0700

    Add test to convert person age to Mercury years

--- __tests__/agechecker.test.js
+++ __tests__/agechecker.test.js
@@ -10,4 +10,7 @@ describe('Person', () => {
   test("should create a person object with the person's age", () => {
     expect(person.age).toEqual(12);
   });
+  test("should return the age of person in Mercury years,", () => {
+    expect(person.ageToMercuryYears()).toEqual(2.88);
+  });
 });
