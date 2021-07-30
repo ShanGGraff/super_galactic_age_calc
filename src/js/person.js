ce61246c718540080a2242df96df62e5e8821c5d
Shane Graff <copellius@gmail.com>
2021-07-29 23:30:49 -0700

    Add method to Person class to convert age to Mercury age

--- src/js/person.js
+++ src/js/person.js
@@ -2,4 +2,8 @@ export default class Person {
   constructor (age) {
     this.age = age;
   }
+  ageToMercuryYears() {
+    let mercuryConversion = .24;
+    return this.age * mercuryConversion;
+  }
 }
