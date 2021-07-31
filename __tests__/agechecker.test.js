e5fb6683ccbee1c113b241a46bb4e2bccb67dea9
Shane Graff <copellius@gmail.com>
2021-07-31 09:17:41 -0700

    Add test for life expectancy on Venus method

--- __tests__/agechecker.test.js
+++ __tests__/agechecker.test.js
@@ -26,4 +26,7 @@ describe('Person', () => {
   test("should return the life expectancy on Mercury", () => {
     expect(person.lifeExpectancyOnMercury()).toEqual(14.40);
   });
+  test("should return the life expectancy on Venus", () => {
+    expect(person.lifeExpectancyOnVenus()).toEqual(37.2);
+  });
 });
